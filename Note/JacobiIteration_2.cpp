7bc5a2063018aca582003346325ccd65c8068d70
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-09-28 21:29:46 +0600

    jacobian iteration (equation input) done

--- Note/JacobiIteration_2.cpp
+++ Note/JacobiIteration_2.cpp
@@ -1,7 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
 #include <stdio.h>
-#include <string.h>
+#include <strings.h>
 using namespace std;
 
 #define a 0
@@ -9,14 +10,17 @@ using namespace std;
 #define c 2
 #define d 3
 
+#define eq1 M[0]
+#define eq2 M[1]
+#define eq3 M[2]
+
 // example
 // a1 = 2.0f, b1 = 1.0f, c1 = 1.0f, d1 = 5.0f;
 // a2 = 3.0f, b2 = 5.0f, c2 = 2.0f, d2 = 15.0f;
 // a3 = 2.0f, b3 = 1.0f, c3 = 4.0f, d3 = 8.0f;
 
 // coEfficientMatrix[num_equations][num_coEfficients]
-int coEfficientMatrix[3][4];
-#define M coEfficientMatrix
+int M[3][4]; // coEfficientMatrix
 
 float x1 = 0.0f, x2 = 0.0f, x3 = 0.0f;
 float x1_prev = 0.0f, x2_prev = 0.0f, x3_prev = 0.0f;
@@ -27,31 +31,53 @@ void take_three_input() {
   // test case -3x1+55x2-2x3=-5
   for (int eq_no = 0; eq_no < 3; eq_no++) { // take three equation input
 
-    char input_eq[20];
-    int left_idx = -1, str_len = strlen(input_eq);
-    int abcd = 0; // co-efficient tracker
+    char input_eq[40] = {0};
+    int abcd = 0; // co-efficient tracker within row of 2D array
     scanf("%[^\n]", input_eq);
 
-    for (int i = 0; input_eq[i] != '\0'; i++) { // catch all coefficients
+    int prev_idx = 0, str_len = strlen(input_eq);
+    /* prev_idx points starting of a substring holding co-efficient*/
 
-      if ((input_eq[i] == 'x' && input_eq[i + 1] == ('1' + i))) {
+    for (int i = 0; i < str_len; i++) { // catch all coefficients
 
-        M[eq_no][abcd] = atoi(strncpy(input_eq, &input_eq[sign_idx], str_len));
+      if (input_eq[i] == 'x' || input_eq[i] == '=') {
 
-        printf("..%d..", M[eq_no][abcd]);
+        char co_eff[10] = "1";
+
+        if (input_eq[i] == '=') { /* edge case */
+          prev_idx++;  /*starting of substring will be position after = sign*/
+          i = str_len; /*ending of substring will be last of the string*/
+        }
+
+        int co_eff_len = (i - prev_idx);
+
+        /* case like x1+x2+x3=5*/
+        if (co_eff_len == 1 && input_eq[prev_idx] == '+') {
+          strncpy(co_eff, "1", 1);
+
+          /* case like -x1-x2-x3=-5*/
+        } else if (co_eff_len == 1 && input_eq[prev_idx] == '-') {
+          strncpy(co_eff, "-1", 2);
+
+        } else {
+          strncpy(co_eff, &input_eq[prev_idx], co_eff_len);
+        }
+
+        M[eq_no][abcd] = (float)atoi(co_eff);
+
         abcd++;
-        left_idx = i;
-        i += 2;
+        prev_idx = i + 2;
+        i++; /*to skip variable name (x1, x2, x3)*/
       }
     }
 
     getchar(); // dummy
   }
-}
+} // working
 
 int main(void) {
 
-  printf("input three equations (in 3x1+5x2-2x3=-5 style, as a string) :\n");
+  printf("input three equations (in x1+x2-2x3=-5 style, as a string) :\n");
   take_three_input(); // limitation: spaces,float/double will not work
 
   printf("input iteration limit : ");
@@ -61,9 +87,9 @@ int main(void) {
 
   for (int i = 1; i <= n; i++) {
 
-    x1 = (M[0][d] - (M[0][b] * x2_prev) - (M[0][c] * x3_prev)) / M[0][a];
-    x2 = (M[1][d] - (M[1][c] * x3_prev) - (M[1][a] * x1_prev)) / M[1][b];
-    x3 = (M[2][d] - (M[2][a] * x1_prev) - (M[2][b] * x2_prev)) / M[2][c];
+    x1 = (eq1[d] - (eq1[b] * x2_prev) - (eq1[c] * x3_prev)) / eq1[a];
+    x2 = (eq2[d] - (eq2[c] * x3_prev) - (eq2[a] * x1_prev)) / eq2[b];
+    x3 = (eq3[d] - (eq3[a] * x1_prev) - (eq3[b] * x2_prev)) / eq3[c];
 
     printf("\nIteration no %d:\tx1 = %f, x2 = %f, x3 = %f\n", i, x1, x2, x3);
 
@@ -81,51 +107,3 @@ int main(void) {
   }
   printf("\n\nProgram Completed");
 } // INCOMPLETE
-
-// ======================================================================
-
-/*
-ADVANCED
-
-void take_three_input() {
-  // test case -3x1+5x2-2x3=-5
-  for (int eq_no = 0; eq_no < 3; eq_no++) { // take three equation input
-
-    char input_eq[20];
-    bool isNegative = false;
-    int sign_idx = -1, prev_idx = -1;
-    int abcd = 0; // co-efficient tracker
-    scanf("%[^\n]", input_eq);
-
-    for (int i = 0; input_eq[i] != '\0'; i++) { // catch all coefficients
-
-      if (input_eq[i] == '-') {
-        sign_idx = i;
-        isNegative = true;
-      }
-
-      if (input_eq[i] == 'x' && input_eq[i + 1] == ('1' + i)) {
-
-        M[eq_no][abcd] =
-            atoi(strncpy(input_eq, &input_eq[sign_idx + 1], strlen(input_eq)));
-
-        if (isNegative) {
-          M[eq_no][abcd] *= -1;
-          isNegative = false;
-        }
-        printf("..%d..", M[eq_no][abcd]);
-        abcd++;
-        prev_idx = i;
-      }
-      if (input_eq[i] == '=') {
-        sign_idx = i;
-        M[eq_no][abcd] =
-            atoi(strncpy(input_eq, &input_eq[sign_idx + 1], strlen(input_eq)));
-        printf("..%d..", M[eq_no][abcd]);
-      }
-    }
-
-    getchar(); // dummy
-  }
-}
-*/
