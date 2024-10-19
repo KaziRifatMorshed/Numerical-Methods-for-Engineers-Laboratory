d47fbc301a9261d0afae8c38c51b2956603a739c
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-10-20 01:44:12 +0600

    Create EulerODE.cpp

---
+++ PersonalPractice/EulerODE.cpp
@@ -0,0 +1,13 @@
+
+#include <cstdio>
+float x0 = 0.0f, y0 = 0.0f, x = 0.0f, y = 0.0f, h = 0.0f;
+int n = 0;
+
+float f(float x, float y) { return 0; }
+
+int main(void) {
+  printf("input h : ");
+  scanf("%f", &h);
+  printf("input x0 and y0 : ");
+  scanf("%f %f", &x0, &y0);
+}
