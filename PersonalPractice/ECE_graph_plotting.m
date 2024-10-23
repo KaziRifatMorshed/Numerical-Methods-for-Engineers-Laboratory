2b2ed1e23d9643e6435218b458c4d515894e8337
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-10-24 00:25:07 +0600

    Hurrah

--- PersonalPractice/ECE_graph_plotting.m
+++ PersonalPractice/ECE_graph_plotting.m
@@ -1,42 +1,25 @@
 % Constants
-R_F = 1000;
-R_l = 1000;
-f = [0:10:100000]; % x-axis
-order = [1:1:6];
-f_c = 1000; %y_axis_low
-A_F = 1 + (R_F / R_l);
-colors = ['r', 'g', 'b', 'c', 'm', 'k'];
-
-% Low Pass Filter
-% V_out_by_V_in_low = A_F * (1 ./ sqrt(1 + (f / f_c) .^ (2)));
-% y_axis_low = 20 * log(V_out_by_V_in_low);
+R_F = 15915; % R1  = R_F = 15.915 kΩ
+R_1 = 15915; % R1  = R_F = 15.915 kΩ
+f = [0:10:100000];
+order = [1:1:5];
+f_c = 10000; % cut off frequency
+A_F = 1 + (R_F / R_1);
+colors = ['r', 'g', 'b', 'm', 'k'];
 
 hold on;
-grid on
+grid on;
 
-% plot(f, y_axis_low)
-% loglog(f, y_axis_low, "r")
+% Low Pass Filter
 xlabel('f (Hz)')
-ylabel('20log(V_o/V_in)')
-title('Frequency Response of 1th to 6th Order LPF')
+ylabel('20 log(V_o/V_in)')
+title('Frequency Response of LPF of Order 1 to 5');
 
-for i = 1:6
-    V_out_by_V_in_low = A_F * (1 ./ sqrt(1 + (f / f_c) .^ (2 * i)));
-    y_axis_low = 20 * log(V_out_by_V_in_low);
-    loglog(f, y_axis_low, colors(i));
+for i = 1:5
+    V_out_by_V_in_LPF = A_F ./ sqrt(1 + ((f / f_c) .^ (2 * i)));
+    y_axis_LPF = 20 * log10(V_out_by_V_in_LPF);
+    loglog(f, y_axis_LPF, colors(i));
 end
 
+legend('Order 1', 'Order 2', 'Order 3', 'Order 4', 'Order 5', 'off');
 hold off;
-
-% High Pass Filter
-
-% title('Frequency Response of 1th to 6th Order HPF')
-% hold on;
-
-% for i = 1:6
-%     V_out_by_V_in_high = A_F * (1 ./ sqrt(1 + (f_c / f) .^ (2 * i)));
-%     y_axis_low = 20 * log(V_out_by_V_in_high);
-%     loglog(f, y_axis_low, colors(i));
-% end
-
-% hold off;
